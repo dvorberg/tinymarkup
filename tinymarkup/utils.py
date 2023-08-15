ce5143e6364d76e42f89955268f263279aba4251
Diedrich Vorberg <diedrich@tux4web.de>
2023-08-15 09:29:44 +0200

    Moved *_remainder() here.

--- tinymarkup/utils.py
+++ tinymarkup/utils.py
@@ -1,19 +1,23 @@
 # Copyright (C) 2023 Diedrich Vorberg
-
+#
 # Contact: diedrich@tux4web.de
-
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
-
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 
 
-import html
+import html, copy, dataclasses
+
+import ply.lex
+
+from .exceptions import Location
 
 def html_start_tag(tag, **params):
     def fixkey(key):
@@ -30,3 +34,16 @@ def html_start_tag(tag, **params):
         params = ""
 
     return f"<{tag}{params}>"
+
+
+def get_remainder(lexer:ply.lex.Lexer) -> str:
+    return lexer.lexdata[lexer.lexpos:]
+
+def set_remainder(lexer:ply.lex.Lexer, remainder:str):
+    if not lexer.lexoptimize:
+        assert lexer.lexdata.endswith(remainder)
+
+    lexer.lexpos = len(lexer.lexdata)-len(remainder)
+
+def get_location(lexer:ply.lex.Lexer) -> Location:
+    return Location.from_baselexer(lexer)
